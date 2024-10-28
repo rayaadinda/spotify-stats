9ccb211764a6f4d9aa54936cd46454a6994bc4c4
rayaadinda <rayaadinda78@gmail.com>
2024-10-28 22:34:35 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 "use client"
 
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SpotifyTopTracks from "./components/SpotifyTopTracks"
 import Footer from "./components/Footer"
 
 function App() {
 	return (
-		<div className="container mx-auto py-8">
-			<SpotifyTopTracks />
-			<Footer />
-		</div>
+		<Router>
+			<div className="container mx-auto py-8">
+				<Routes>
+					<Route path="/" element={<SpotifyTopTracks />} />
+					<Route path="/callback" element={<SpotifyTopTracks />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
 	)
 }
 
