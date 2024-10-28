5d9eef3062340d7cb612ff2cf263e5f91c57704e
rayaadinda <rayaadinda78@gmail.com>
2024-10-29 01:47:48 +0700

    fix the export

--- src/lib/exportImage.js
+++ src/lib/exportImage.js
@@ -1,10 +1,7 @@
 import * as htmlToImage from "html-to-image"
 
-export const exportAsImage = async (element, fileName, onProgress) => {
+export const exportAsImage = async (element, fileName) => {
 	try {
-		// Start progress
-		onProgress(10)
-
 		// Wait for images to load
 		const images = element.getElementsByTagName("img")
 		const imagePromises = Array.from(images).map((img) => {
@@ -19,40 +16,28 @@ export const exportAsImage = async (element, fileName, onProgress) => {
 		})
 
 		await Promise.all(imagePromises)
-		onProgress(40)
 
 		// Generate image
 		const dataUrl = await htmlToImage.toPng(element, {
 			quality: 1.0,
 			backgroundColor: null,
-			pixelRatio: 4,
-			width: element.offsetWidth * 2,
-			height: element.offsetHeight * 2,
-			skipAutoScale: false,
+			pixelRatio: 3,
+			skipAutoScale: true,
 			style: {
 				transform: "none",
-				transformOrigin: "center",
-				width: `${element.offsetWidth * 2}px`,
-				height: `${element.offsetHeight * 2}px`,
 			},
 			filter: (node) => {
+				// Skip elements with certain classes if needed
 				return !node.classList?.contains("exclude-from-image")
 			},
-			canvasWidth: element.offsetWidth * 2,
-			canvasHeight: element.offsetHeight * 2,
 		})
-
-		onProgress(80)
 
 		// Download image
 		const link = document.createElement("a")
 		link.download = `${fileName}.png`
 		link.href = dataUrl
 		link.click()
-
-		onProgress(100)
 	} catch (error) {
 		console.error("Error exporting image:", error)
-		onProgress(0)
 	}
 }
